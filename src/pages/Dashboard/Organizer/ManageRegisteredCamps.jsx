9d8718ac1d9be9329b64be40a8d625fc3e3b36f8
minhaj.dev <minhajhasan.dev@gmail.com>
2024-06-12 19:27:53 +0600

    added user data row

--- src/pages/Dashboard/Organizer/ManageRegisteredCamps.jsx
+++ src/pages/Dashboard/Organizer/ManageRegisteredCamps.jsx
@@ -1,10 +1,94 @@
+import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
+
+import UserDataRow from "../../../components/Dashboard/TableRows/UserDataRow";
 const ManageRegisteredCamps = () => {
-    return (
-        <div>
-            <p>This is manage reg camps</p>
+  const axiosSecure = useAxiosSecure();
+  //   Fetch  Data
+  const {
+    data: bookings = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["bookings"],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/bookings`);
+      return data;
+    },
+  });
+
+  console.log(bookings);
+  if (isLoading) return <LoadingSpinner />;
+  return (
+    <>
+      <div className="container mx-auto px-4 sm:px-8">
+        <Helmet>
+          <title>Manage Users</title>
+        </Helmet>
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Participant Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Camp Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Camp Fees
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Payment Status
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Confirmation Status
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                    >
+                      Cancel
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {bookings.map((booking) => (
+                    <UserDataRow
+                      key={booking?._id}
+                      booking={booking}
+                      refetch={refetch}
+                    />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default ManageRegisteredCamps;
