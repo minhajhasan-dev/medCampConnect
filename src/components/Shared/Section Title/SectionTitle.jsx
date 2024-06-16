72ceb1e2bf3e54c88f0732ebaa446a2c080a88be
minhaj.dev <minhajhasan.dev@gmail.com>
2024-06-16 23:40:45 +0600

    animated the section title

--- src/components/Shared/Section Title/SectionTitle.jsx
+++ src/components/Shared/Section Title/SectionTitle.jsx
@@ -2,7 +2,11 @@ import PropTypes from "prop-types";
 
 const SectionTitle = ({ headline }) => {
   return (
-    <div className="text-center mt-0 ">
+    <div
+      data-aos="zoom-in"
+      data-aos-duration="1000"
+      className="text-center mt-0 "
+    >
       <h1 className="md:text-3xl text-xl font-semibold">{headline}</h1>
     </div>
   );
